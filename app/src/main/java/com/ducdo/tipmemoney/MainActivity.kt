2833d356991aa2008c06a0087d1b9452b9770c9b
rootofmylife <dotrananhduc@gmail.com>
2021-09-21 02:01:55 +0700

    Update theme and icon for app

--- app/src/main/java/com/ducdo/tipmemoney/MainActivity.kt
+++ app/src/main/java/com/ducdo/tipmemoney/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.ducdo.tipmemoney
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.ducdo.tipmemoney.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -14,10 +18,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.calculateButton.setOnClickListener{ calculateTip() }
+
+        binding.costOfServiceEditText.setOnKeyListener {
+                view, keyCode, _ -> handleKeyEvent(view, keyCode)
+        }
     }
 
     private fun calculateTip() {
-        val stringInTextField = binding.costOfService.text.toString()
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
         if (cost == null) {
             binding.tipResult.text = ""
@@ -34,5 +42,16 @@ class MainActivity : AppCompatActivity() {
         }
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
